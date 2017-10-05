1ba45aede0a9181103f4c7ca1f60678d8d5cd0b8
mostafakhattab00 <mostafa.khattab@ibtikar.net.sa>
2017-10-05 13:29:20 +0200

    adding the application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 122},
+    {title: 'The Dark Tower', pages: 500},
+    {title: 'Eloquent ruby', pages: 1200}
+  ];
+}
